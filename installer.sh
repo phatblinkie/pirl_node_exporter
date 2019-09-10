d63a542ffc0d1b613ae0c3f85b80f2ebb832b4a2
phatblinkie <phatblinkie@hotmail.com>
2019-09-09 22:12:56 -0400

    path fix

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@ then
 fi
 
 useradd -m -s /bin/bash prometheus  
-https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+wget -O node_exporter-0.18.1.linux-amd64.tar.gz https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
 tar -zxvf node_exporter-0.18.1.linux-amd64.tar.gz
 mv -f node_exporter-0.18.1.linux-amd64/node_exporter /usr/bin/
 chmod 0755 /usr/bin/node_exporter
